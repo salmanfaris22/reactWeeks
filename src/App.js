504c3b322b688730b89e59e95eb65416c15bafc5
salmanfaris22 <salman567633800@gmail.com>
2024-07-17 09:01:52 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+import About from './Components/about/about';
+import Hero from './Components/Hero/Hero';
+
+function App() {
+  return (
+    <div className="App">
+      <Hero/>
+      <About/>
+    </div>
+  );
+}
+
+export default App;
