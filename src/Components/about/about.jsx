4bddd8533f68447247d809092e29e9dd6c122f17
salmanfaris22 <salman567633800@gmail.com>
2024-07-17 10:32:43 +0530

    first commit

--- src/Components/about/about.jsx
+++ src/Components/about/about.jsx
@@ -1,11 +1,31 @@
-import React from 'react'
+import React, { useState } from 'react'
 import "./about.css"
 
 
 
 const About = () => {
+  
+const [num, setNum] = useState(0)
+
+const ingrement = ()=>{
+  setNum(num+1)
+}
+const decrement = ()=>{
+  setNum(num-1)
+}
+const reset = ()=>{
+  setNum(0)
+}
+
   return (
-    <div className='about-main'>about</div>
+    <div className='about-main'>
+      <div>{num}</div>
+      <div>
+        <button onClick={ingrement}>Ingrement</button>
+        <button onClick={decrement}>Decrement</button>
+        <button onClick={reset}>Reset</button>
+      </div>
+    </div>
   )
 }
 
