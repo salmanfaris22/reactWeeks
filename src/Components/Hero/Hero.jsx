b769c91f4ae01c0c0f408bd32a4c18be992bbd0b
salmanfaris22 <salman567633800@gmail.com>
2024-07-17 10:23:26 +0530

    first commit

--- src/Components/Hero/Hero.jsx
+++ src/Components/Hero/Hero.jsx
@@ -1,10 +1,51 @@
-import React from 'react'
+
+import React, { useState } from 'react'
 import './hero.css'
 
 
 const Hero = () => {
+
+  const [name, setName] = useState("jhone");
+  function newName() {
+    setName("shilly")
+  }
+
+
+  const [age, setAge] = useState(1)
+  function ageNow() {
+    setAge(age + 1)
+  }
+
+
+  function resetAll() {
+    setAge(0)
+    setName("jhone")
+  }
+
+ const [employ , setEmploy] = useState(false)
+
+ function employeCheck(){
+    setEmploy(!employ)
+ }
+
   return (
-    <div className='hero-main'>Hero</div>
+    <div className='hero-main'>
+      <div>
+        <h1>{name}</h1>
+        <button onClick={newName}>Name</button>
+      </div>
+      <div>
+        <h1>{age}</h1>
+        <button onClick={ageNow}>Name</button>
+      </div>
+   <div>
+      <h1>{employ ?  "yes":"no"}</h1>
+      <button onClick={employeCheck}>is Toggle</button>
+   </div>
+      <div>
+        <button onClick={resetAll}>Reset</button>
+      </div>
+    </div>
   )
 }
 
