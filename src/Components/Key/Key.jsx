362e581d65afd98568625e06eb18f8e7d22a3580
salmanfaris22 <salman567633800@gmail.com>
2024-07-18 10:24:53 +0530

    first commit

--- src/Components/Key/Key.jsx
+++ src/Components/Key/Key.jsx
@@ -1,25 +1,46 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 
 
 const Key = () => {
 
 const items= [
-    {name:"laptop",price:1000,qty:1},
-    {name:"Hp",price:2000,qty:1},
-    {name:"Dell",price:1000,qty:1},
-    {name:"lap",price:1090,qty:1},
+    {id:1,name:"Hp",price:2000,qty:1},
+    {id:2,name:"Dell",price:1000,qty:1},
+    {id:3,name:"lap",price:1090,qty:1},
 ]
 
+const [qtys, setQyts] = useState(items)
+
+let onChange = (id)=>{
+  let newItem =   qtys.map((ite)=>{
+        return (
+            ite.id === id ? {...ite, qty: ite.qty+1} : ite
+        )
+    })
+   setQyts(newItem) 
+}
+
+let onChanges = (id)=>{
+    let newItem =   qtys.map((ite)=>{
+          return (
+              ite.id === id ? {...ite, qty: ite.qty-1} : ite
+          )
+      })
+     setQyts(newItem) 
+  }
   return (
+
+
     <div style={{height:"100vh"}}>
-        {items.map((item)=>{
+        {qtys.map((item)=>{
           return  (
-            <div className='box'>
+            <div className='box' key={item.id}>
             <h1>{item.name}</h1>
             <h3>{item.price}</h3>
             <h3>{item.qty}</h3>
-            <button>+</button>
+            <button onClick={()=>onChange(item.id)}>+</button>
+            <button onClick={()=>onChanges(item.id)}>-</button>
             
             </div>
           )
