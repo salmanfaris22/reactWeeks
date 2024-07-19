096e8443846f9270a8d6f4c86a28f5cb090289cc
salmanfaris22 <salman567633800@gmail.com>
2024-07-19 19:39:31 +0530

    first commit

--- src/Components/Todo.jsx/Todo.jsx
+++ src/Components/Todo.jsx/Todo.jsx
@@ -2,84 +2,130 @@ import React, { useState } from 'react'
 
 const Todo = () => {
 
-const [task,SetTask]  = useState(["eat breakfaset","eart one"])
-const [newTask, setNewTask] = useState("")
-const [editIndex ,setEditIndex]=useState(null)
+    const [task, SetTask] = useState([])
+    const [newTask, setNewTask] = useState("")
+    const [editIndex, setEditIndex] = useState(null)
+    const [doneTask, setDonetask] = useState([])
 
-function handleChange(e){
-     setNewTask(e.target.value)
-     setEditIndex(e.target.value)
-    
-}
 
-function handleGetData(){
-    if(newTask.trim().length !==0){
-        console.log(newTask);
-        SetTask([...task,newTask])
+    function handleChange(e) {
+        setNewTask(e.target.value)
+
+    }
+
+    function handleGetData() {
+        if (newTask.trim().length !== 0) {
+            console.log(newTask);
+            SetTask([...task, newTask])
+            setNewTask("")
+        }
+
+
+
+    }
+    function handleEditData(e) {
+
+
+        task[editIndex] = newTask
+        handleEdite()
         setNewTask("")
+        setEditIndex(null)
+
+    }
+    function handleEdite(index) {
+
+        setNewTask(task[index])
+        setEditIndex(index)
+
+
+    }
+    function deletTask(index) {
+        const updatedTask = task.filter((task, i) => i !== index)
+        SetTask(updatedTask)
     }
 
 
-   
-}
-function handleEditData(e){
-
- 
-}
-function handleEdite(index){
-     
-
- 
+    function handleCompleat(index) {
+        const moveCompleat = task.filter((task, i) => i === index)
+        deletTask(index)
+        setDonetask([...doneTask, moveCompleat])
 
 
-}
-function deletTask(index){
-   const updatedTask = task.filter((task , i)=> i !== index)
-   SetTask(updatedTask)
-}
-
+    }
 
     return (
-        <div className='flex items-center justify-center h-[100vh]'>
-            <div className='w-[50%] h-[50%] bg-orange-200 rounded-xl flex flex-col items-center'>
+        <div className='flex flex-col  h-[100vh]'>
+            <div className='w-[100%] h-[20%] bg-orange-200 rounded-xl flex flex-col justify-center items-center'>
                 <div>
-                    <input type="text" className='mt-6 p-3 w-60 rounded-xl'  
-                    name='data' 
-                    // value={getData} 
-                    onChange={handleChange} 
-                    value={newTask}
-                    placeholder='Enter Your Todo' 
-                    id='task'
-                    autoFocus
+                    <input type="text" className=' p-3 w-[600px] rounded-xl'
+                        name='data'
+                        // value={getData} 
+                        onChange={handleChange}
+                        value={newTask}
+                        placeholder='Enter Your Todo'
+                        id='task'
+                        autoFocus
                     />
-                   {
-                    editIndex === null ?  <button onClick={handleGetData} className='ml-4'
-                    type='submit'
-                    >ADD</button>:
-                    <button onClick={handleEditData} className='ml-4'
-                    type='edit'
-                    >Eidit</button>
-                   }
+                    {
+                        editIndex === null ? <button onClick={handleGetData} className='ml-4'
+                            type='submit'
+                        >ADD</button> :
+                            <button onClick={() => handleEditData()} className='ml-4'
+                                type='edit'
+                            >Eidit</button>
+                    }
                 </div>
 
-                <div>
-                   {
-                    task.map((e,index)=>{
-                        return (
-                            <div className='flex mt-3 gap-1' key={index}>
-                            <div className='p-2 w-[300px]   bg-blue-50 rounded-lg' >{e}</div>
-                            <button className='h-[50px] flex  text-center  justify-center items-center  p-2'
-                            onClick={()=>deletTask(index)}
-                            >delet</button>
-                            <button onClick={()=>handleEdite(index)}>Edit</button>
-                            <button>Compleat</button>
-                        </div>
-                        )
-                    })
-                   }
-                   
-                </div>
+
             </div>
+
+            <div>
+
+                <div className=' w-[100%] h-[500px] grid items-center grid-rows-2 grid-flow-col' >
+                    {
+                        task.map((e, index) => {
+                            return (
+
+
+                                <div key={index} className='w-[300px] h-[200px]  ml-10 flex flex-col justify-between items-center rounded-xl shadow-md shadow-black'>
+                                    <div className='w-[200px] mt-6 h-[130px]'>
+                                        {e}
+                                    </div>
+                                    <div className='flex h-[20%] items-center  '>
+                                        <button className='h-5 flex rounded  shadow-sm shadow-black bg-black font-extralight justify-center items-center'
+                                            onClick={() => deletTask(index)}
+                                        >Delet</button>
+                                       <button className='h-5 flex rounded  shadow-sm shadow-black bg-black font-extralight justify-center items-center'
+                                        onClick={() => handleEdite(index)}>Edit</button>
+                                       <button className='h-5 flex rounded  shadow-sm shadow-black bg-black font-extralight justify-center items-center'
+                                        onClick={() => handleCompleat(index)}>Compleat</button>
+                                    </div>
+                                </div>
+
+                                //     <div className='flex mt-3 gap-1' key={index}>
+                                //     <div className='p-2 w-[300px]   bg-blue-50 rounded-lg' >{e}</div>
+                                //     <button className='h-[50px] flex  text-center  justify-center items-center  p-2'
+                                //     onClick={()=>deletTask(index)}
+                                //     >delet</button>
+                                //     <button onClick={()=>handleEdite(index)}>Edit</button>
+                                //     <button onClick={()=>handleCompleat(index)}>Compleat</button>
+                                // </div>
+                            )
+                        })
+                    }
+
+                </div>
+
+            </div>
+            {
+                doneTask.map((done, did) => {
+                    return (
+                        <div key={did}>
+                            <div>{done}</div>
+                        </div>
+                    )
+                })
+            }
         </div>
     )
 }
