75ef4f4d5e45bfc154fe7cf828b832b6cdd6f65f
salmanfaris22 <salman567633800@gmail.com>
2024-07-20 10:33:13 +0530

    first commit

--- src/Components/Todo.jsx/Todo.jsx
+++ src/Components/Todo.jsx/Todo.jsx
@@ -1,6 +1,5 @@
 import React, { useState } from 'react'
 import DeletedTasks from './DeleteTAsks/DeletedTasks'
-import ToDoinput from './ToDoinput/ToDoinput'
 
 const Todo = () => {
 
@@ -80,9 +79,28 @@ const Todo = () => {
         <div className='flex flex-col  h-[100vh] bg-[#101820]'>
             <div className='w-[100%] h-[20%] rounded-xl flex flex-col justify-center items-center'>
                 <div>
-                  <ToDoinput/>
+                    {/* input felid */}
+                    <input type="text" className=' p-3 w-[600px] shadow-sm shadow-black rounded-xl'
+                        name='data'
+                        // value={getData} 
+                        onChange={handleChange}
+                        value={newTask}
+                        placeholder='Enter Your Todo'
+                        id='task'
+                        autoFocus
+                    />
+                    {/* ADD EDIT BUTTON */}
+                    {
+                        editIndex === null ? <button onClick={handleGetData} className='ml-4 bg-white text-black font-bold'
+                            type='submit'
+                        >ADD</button> :
+                            <button onClick={() => handleEditData()} className='ml-4'
+                                type='edit'
+                            >Eidit</button>
+                    }
                     <button className=' absolute right-0  bg-black' onClick={handelDeletOpen}>DeletedTasks</button>
                     <div className='absolute right-0 '>
+                        {/* deleted BOX DOTOS */}
                         {deletOpen && <DeletedTasks deletedTask={deletedTask} handelRestoreTask={handelRestoreTask}/>}
 
                     </div>
@@ -93,7 +111,7 @@ const Todo = () => {
             </div>
 
             <div>
-
+{/* TODO main Aded ToDO lists */}
                 <div className='w-[100%] h-[500px] grid items-center grid-rows-2 bg-[#101822] grid-flow-col overflow-auto' onClick={() => setDeletOpen(false)}>
                     {
                         task.map((e, index) => {
@@ -115,14 +133,7 @@ const Todo = () => {
                                     </div>
                                 </div>
 
-                                //     <div className='flex mt-3 gap-1' key={index}>
-                                //     <div className='p-2 w-[300px]   bg-blue-50 rounded-lg' >{e}</div>
-                                //     <button className='h-[50px] flex  text-center  justify-center items-center  p-2'
-                                //     onClick={()=>deletTask(index)}
-                                //     >delet</button>
-                                //     <button onClick={()=>handleEdite(index)}>Edit</button>
-                                //     <button onClick={()=>handleCompleat(index)}>Compleat</button>
-                                // </div>
+                             
                             )
                         })
                     }
@@ -133,7 +144,7 @@ const Todo = () => {
             <br />
             <div className='ml-9 mt-10 text-2xl font-bold text-white'>Compleated TASK</div>
             <br />
-
+ {/* Compleated Task */}
             <div className=' w-[100%] h-[450px] grid items-center grid-rows-2 grid-flow-col overflow-auto' >
                 {
                     doneTask.map((done, did) => {
