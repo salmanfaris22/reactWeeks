b85eade219a855c5c8f74e41db0190c6732314c0
salmanfaris22 <salman567633800@gmail.com>
2024-07-28 21:27:38 +0530

    first commit

--- src/Component2/Usereducer/Reduce.jsx
+++ src/Component2/Usereducer/Reduce.jsx
@@ -1,8 +1,40 @@
-import React from 'react'
+import React, { useReducer } from 'react'
+
+
+
+
+function reducer(prestate,action){
+    // eslint-disable-next-line default-case
+    switch (action){
+        case "increment":
+            return {
+                ...prestate,
+                number:prestate.number+1
+            }
+        case "red" :
+            return {
+                ...prestate,
+                color:"red"
+            }     
+        case "green" :
+            return {
+                ...prestate,
+                color:"green"
+            }
+
+    }
+}
 
 const Reduce = () => {
+    const initialState = { number: 0, color: "red" };
+    const [state, dispatch] = useReducer(reducer, initialState);
   return (
-    <div>Reduce</div>
+    <div style={{color:state.color}}>
+        {state.number}
+        <button onClick={()=>dispatch("increment")}>ingrement</button>
+        <button onClick={()=>dispatch("green")}>green</button>
+        <button onClick={()=>dispatch("red")}>red</button>
+    </div>
   )
 }
 
